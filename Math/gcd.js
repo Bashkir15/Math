585e4d03181230e07741fbd5a7427bd6d2c6d796
Forrest Collins <Forest.d.collins@gmail.com>
2016-11-13 03:09:03 -0600

    Added a bianary version of the greatest common divisor

--- Math/gcd.js
+++ Math/gcd.js
@@ -13,3 +13,56 @@ var gcdDivision = (a, b) => {
 
 	return a;
 };
+
+var gcdBinary = (a, b) => {
+	var shift;
+	var tmp;
+
+	if (a === 0) {
+		return b;
+	}
+
+	if (b === 0) {
+		return a;
+	}
+
+	// Let shift = log(K), where K is the greatest power of 2
+	// then dividing by both a and b
+
+	for (shift = 0; ((a | b) & 1) === 0; shift++) {
+		a >>= 1;
+		b >>= 1;
+	}
+
+	// remove all factors of 2 in a -- they wil not be common
+	while ((a & 1) === 0) {
+		a >>= 1;
+	}
+
+	// a is always odd
+
+	do {
+
+		// remove all factors of 2 in b -- they will not be common
+		while ((b & 1) === 0) {
+			b >>= 1;
+		}
+
+		// now a and b are both odd
+		if (a > b) {
+			tmp = b;
+			b = a;
+			a = tmp;
+		}
+
+		b -= a;
+	} while (b !== 0);
+
+	//restore common factors of 2
+	return a << shift;
+};
+
+module.exports = {
+	gcdDivision: gcdDivision,
+	gcdBinary: gcdBinary
+};
